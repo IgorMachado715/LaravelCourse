42e129cc9880a9295245aae08d9714889c7c33f8
Igor Machado <igor.gabriel.machado@gmail.com>
2024-06-18 00:37:27 -0300

    initial commit-first Controller/route/view

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Create Events')
+
+@section('content') 
+
+<h1 class="text-3xl font-bold">
+    Create event
+</h1>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Events Laravel')
+
+@section('content') 
+
+
+
+  <h1 class="text-3xl font-bold">
+    Hello world!
+  </h1>
+
+
+
+
+@endsection
